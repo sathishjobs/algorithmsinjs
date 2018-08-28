4f1bc3040aaf9a2e98684fb16d259293ead1bdaf
sathish kumar <sathishkumarcisco@gmail.com>
2018-08-28 17:38:54 +0530

    plaindrome problem added

---
+++ isplaindrome.js
@@ -0,0 +1,18 @@
+function isPalindrome(string){
+    string = string.toLowerCase();
+    var charactersArr = string.split('');
+    var validCharacters ='abcdefghijklmnopqrstuvwxyz'.split('');
+    var lettersArr = [];
+
+    charactersArr.forEach(char=>{
+        if(validCharacters.indexOf(char) > -1) lettersArr.push(char);
+    })
+
+    if(lettersArr.join('') === lettersArr.reverse().join('')) return true;
+    else return false;
+
+}
+
+// console.log(isPalindrome("Madam I'm Adam"));
+// console.log(isPalindrome("Anna"));
+console.log(isPalindrome("sathish kumar"));
