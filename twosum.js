0a95828a61701642da998e145de0e07a22d74638
sathish kumar <sathishkumarcisco@gmail.com>
2018-08-31 12:24:15 +0530

    twosumproblem

---
+++ twosum.js
@@ -0,0 +1,16 @@
+function twoSum(numArray,sum){
+    var pairs = [];
+    var hashTable = [];
+
+    for(var i = 0; i< numArray.length; i++){
+        var currNum = numArray[i];
+        var counterpart = sum - currNum;
+        if(hashTable.indexOf(counterpart) !== -1){
+            pairs.push([currNum,counterpart])
+        }
+        hashTable.push(currNum);
+    }
+    return pairs;
+}
+
+console.log(twoSum([1,6,4,5,3,3],7));
