37ad6e48678e9e54b53c31d8eb8cfe98fcf09d00
sathish kumar <sathishkumarcisco@gmail.com>
2018-09-04 13:25:15 +0530

    sieveoferatosthenes problem solved

--- fibonacci.js
+++ fibonacci.js
@@ -1,6 +1,10 @@
 function fibonacci(position){
     if(position < 3) return 1;
-    else return fibonacci(position -1) + fibonacci(position - 2);
+    else {
+        let result = fibonacci(position -1) + fibonacci(position - 2);
+        return result;
+    }
+    // else return fibonacci(position -1) + fibonacci(position - 2);
 }
 
-console.log(fibonacci(10));
+console.log(fibonacci(6));
