6a42b45459d131f688c2f97d574dceea2c8578d2
sathish kumar <sathishkumarcisco@gmail.com>
2018-09-04 13:57:02 +0530

    bubblesort.js

---
+++ bubblesort.js
@@ -0,0 +1,16 @@
+
+
+function bubbleSort(array){
+    for(var i = array.length;i>0;i--){
+        for(var j = 0;j<i;j++){
+            if(array[j] > array[j+1]){
+                var temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+console.log(bubbleSort([3,-9,-12,-1,8]));
